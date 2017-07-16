21e4b2fdade03606d79cc11d3e5e5bc9e50094bf
Васо Джинчвелашвили <vvdzhinchvelashvili@edu.hse.ru>
2017-07-16 11:53:23 +0300

    db, repo service

---
+++ NosesApi/NosesApi/Repository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NosesApi.App_Data;
+namespace NosesApi
+{
+    public class Repository
+    {
+
+        private readonly DbOperationsEntities _context;
+
+        public Repository()
+        {
+            _context = new DbOperationsEntities();
+        }
+        public void kek()
+        {
+           
+        }
+    }
+}
